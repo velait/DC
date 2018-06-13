1bc6ad6acb90115bd92020694c3025f7deb8d920
velait <villelaitinen@dhcp-238-149.vub.ac.be>
2018-06-13 12:04:16 +0200

    R

--- scripts/start_up.R
+++ scripts/start_up.R
@@ -1,1 +1,26 @@
 library(tidyverse)
+library(lubridate)
+
+
+download.file("https://ndownloader.figshare.com/files/2292169", "data/portal_data_joined.csv")
+
+survey <- read.csv("data/portal_data_joined.csv")
+
+sex <- factor(c("male", "female", "female", "male"))
+lvls <- factor(c("high", "medium", "low"), levels = c("low", "medium", "high"))
+
+as.character(sex)
+
+sex <- survey$sex
+levels(sex) <- c("Undetermined","Female", "Male")
+plot(factor(sex, levels = c("Female", "Male", "Undetermined")))
+
+
+survey <- read.csv("data/portal_data_joined.csv", stringsAsFactors = TRUE)
+
+# Add date column
+survey$date <- ymd(paste(survey$year, survey$month, survey$day,sep="-"))
+
+missing_date <- survey[which(is.na(survey$date)), c("year", "month", "day")]
+
+
